29933948ef01c47fbeede3d02af12c00b1c37785
S_ghanbary98 <67980207+Sherv1360@users.noreply.github.com>
2021-07-07 07:34:45 +0100

    updated README

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Model View Controller (MVC) with Python Flask
+
+
+
+## Python Flask
+
+- Flask is a python micro-framework.
+- Flask is used to develop web apps.
+
+
+### Installing and importing Flask
+- We must first install the module using `pip install flask` at which point we import it on our `app.py` document.
+- Then we initialize the app instance.
+```python
+from flask import Flask, redirect, url_for, render_template
+# We have to create an object of this class
+
+app = Flask(__name__)  # initalizing an app instance.
+```
+
+### Routing Python to browser
+
+- We route the following text in the `index` function so it display in the browser as follows.
+```python
+@app.route("/")  # Decorating our function with @app.route to set our route
+def index():
+    return "Welcome to Engineering 89 DevOps Team"
+```
+### Running the Program
+- We can run the program by typing `flask run`
+
+### Redirecting URL
+- we redirect to another url using the command
--- app.py
+++ app.py
@@ -1,16 +1,50 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Creating a file called app.py
+# Lets see how we can use Flask to interact with the browser.
+# Install Flask via `pip install flask`
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+from flask import Flask, redirect, url_for, render_template
+# We have to create an object of this class
+
+app = Flask(__name__)  # initalizing an app instance.
+
+
+# Let's create a function to link to our home/default page
+# Lets connect this function to our browser.
+
+@app.route("/")  # Decorating our function with @app.route to set our route
+
+def index():
+    return "Welcome to Engineering 89 DevOps Team"
+
+
+@app.route("/welcome/")
+def welcome():
+    return render_template("welcome.html")
+
+
+# Creating a decorator to route traffic.
+
+
+@app.route("/login/")
+
+def login():   # redirect and url_for to redirect pages.
+    # return "<h1> Hello World !!!!!!!! </h1> <h2> Shervin Wrote this</h2>"
+    return redirect(url_for("welcome"))       ## This will redirect user to the welcome page
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+# Let's add our HTML file to reirect away from python flask to .html file.
+# Project folder
+ #template folder
+    # welcome.html
+# app.py
+
+# What if the page is not available?? 404 error given. But we can also control it amd redirect users
